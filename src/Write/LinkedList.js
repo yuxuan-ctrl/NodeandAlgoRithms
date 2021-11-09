6eb30d2a8f5f595b3074e12a6c6ae86d33cd4cdb
yuxuan-ctrl <714180720@qq.com>
2021-11-10 00:17:22 +0800

    xiugai

--- src/Write/LinkedList.js
+++ src/Write/LinkedList.js
@@ -23,12 +23,46 @@ class LinkedList {
     }
     this.length += 1
   }
+  insert(position,data){
+    const newNode = new Node(data);
+    if(position<0 || position>this.length) return false;
+    if(position ===0){
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    else{
+      let currentNode = this.head;
+      let previousNode ;
+      let index = 0;
+      while(index++<position){
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+      }
+      previousNode.next=newNode;
+      newNode.next=currentNode
+    }
+    this.length+=1;
+    return true;
+  }
+  toString(){
+    let listString = '';
+    let currentNode = this.head;
+    if(this.length==0){
+      return ''
+    }
+    else{
+      while(currentNode){
+        listString+=currentNode.data
+        currentNode=currentNode.next
+      }
+      return listString
+    }
+  }
 }
 
 let li = new LinkedList();
 li.append('aaaa')
-console.log(li.length);
 li.append('bbbb')
-li.append('cccc')
-li.append('dddd')
+li.insert(0,"333")
 console.log(li);
+console.log(li.toString());
